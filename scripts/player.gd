b96118f94054aa2bded89f571a36cc96ce3e1459
jake-ciliberti <jakeciliberti@gmail.com>
2026-06-22 00:29:43 -0400

    added initial controller input methods

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+var double_jump_available = true
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,8 +12,13 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			double_jump_available = true
+		elif double_jump_available:
+			velocity.y = JUMP_VELOCITY
+			double_jump_available = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
