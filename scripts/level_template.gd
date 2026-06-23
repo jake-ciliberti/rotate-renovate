3e9ebfb72fc250a8ba54b0e4ee04826ed848e845
jake-ciliberti <jakeciliberti@gmail.com>
2026-06-23 03:07:16 -0400

    added test panels to level

--- scripts/level_template.gd
+++ scripts/level_template.gd
@@ -1,7 +1,9 @@
 extends "level.gd"
 
 @export var testPanel: Node2D
+@export var testPanel2: Node2D
+@export var testPanel3: Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	rotation_groups = [[testPanel]]
+	rotation_groups = [[testPanel], [testPanel2, testPanel3]]
