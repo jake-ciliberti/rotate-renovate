78de18bb97e6dac04ae9d15fd93e005fb1f06391
jake-ciliberti <jakeciliberti@gmail.com>
2026-06-24 16:18:00 -0400

    added level switching

---
+++ scripts/level_container.gd
@@ -0,0 +1,46 @@
+extends Node2D
+
+signal reset_player
+
+var level: Level
+var level_number = 0
+
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("ui_text_backspace"):
+		print("oh yeah")
+		increment_level()
+	
+	pass
+
+func unload_level():
+	if (is_instance_valid(level)):
+		level.queue_free()
+	level = null
+
+func load_level(new_level: int):
+	unload_level()
+	var level_path := "res://levels/level_%s.tscn" % new_level
+	var level_resource := load(level_path)
+	
+	if !(level_resource):
+		level_number = 0
+		return
+	
+	level = level_resource.instantiate()
+	
+	level.reset.connect(_on_level_reset)
+	
+	add_child(level)
+	
+	return
+
+
+func increment_level():
+	load_level(level_number + 1)
+
+func _on_level_reset(spawnpoint: Vector2):
+	reset_player.emit(spawnpoint)
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,26 +1,20 @@
 extends Node2D
 
 @export var player: CharacterBody2D
-@export var level: Level # TODO: figure out how to allow this to swap levels
-
-var level_number = 0
+@export var level_container: Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	level.reset.connect(_on_level_reset)
-	# load_level(level_number) # TODO: doesn't work. fix!
+	level_container.reset_player.connect(_on_level_container_reset_player)
+	level_container.load_level(0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
-func load_level(new_level: int):
-	level_number = new_level
-	
-	level.get_tree().change_scene_to_file("res://levels/level_" + str(level_number) + ".tscn")
+# NOTE: switching levels this way requires changing the z-index of Player. There's probably a better way to do this.
+#func load_level(new_level: int):
+	#level_container.load_level(new_level)
 
-func increment_level():
-	load_level(level_number + 1)
-
-func _on_level_reset(spawnpoint: Vector2):
+func _on_level_container_reset_player(spawnpoint: Vector2):
 	player.teleport(spawnpoint)
