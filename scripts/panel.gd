6e6e2c650184d1600e1ffefcec8259bc3620783e
jake-ciliberti <jakeciliberti@gmail.com>
2026-06-23 22:47:37 -0400

    updated sprites

--- scripts/panel.gd
+++ scripts/panel.gd
@@ -2,6 +2,8 @@
 extends AnimatableBody2D
 
 @export var initial_rotation_degrees: float
+@export var pivot: Vector2
+@export var group: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,6 +12,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+#func rotate(group: int, ):
 
 func reset():
 	rotation_degrees = initial_rotation_degrees
