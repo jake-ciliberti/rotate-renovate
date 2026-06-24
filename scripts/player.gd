622a12900c9b21a231c0a630e7edcf096349b2ca
jake-ciliberti <jakeciliberti@gmail.com>
2026-06-24 09:26:52 -0400

    added ground slam

--- scripts/player.gd
+++ scripts/player.gd
@@ -14,7 +14,7 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
@@ -23,7 +23,7 @@ func _physics_process(delta: float) -> void:
 		elif double_jump_available:
 			velocity.y = JUMP_VELOCITY
 			double_jump_available = false
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
@@ -32,5 +32,15 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	sprite.flip_v = false
+	
+	if Input.is_action_pressed("down"):
+		if Input.is_action_just_pressed("down"):
+			velocity.y = get_gravity().y
+		
+		velocity += 2 * get_gravity() * delta
+		
+		sprite.flip_v = true
 
 	move_and_slide()
