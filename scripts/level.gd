c1749689bf81cf97dbf46fe7d6d068c2ce5bdaa4
jake-ciliberti <jakeciliberti@gmail.com>
2026-06-23 20:44:43 -0400

    fixed parallax scrolling and switching groups

--- scripts/level.gd
+++ scripts/level.gd
@@ -4,7 +4,7 @@ class_name Level extends Node2D
 # @export var player: NodePath
 
 var selected_rotation_group = 0 # TODO: figure out how to move this to player
-@export var rotation_groups: Array[Array]
+var rotation_groups: Array[Array]
 
 var rotation_direction = 0
 
@@ -14,18 +14,17 @@ func _ready() -> void:
 
 func rotate_panels(delta: float) -> void:
 	rotation_direction = Input.get_axis("rotate_counterclockwise", "rotate_clockwise")
-	# TODO: fix
 	for i in rotation_groups[selected_rotation_group]:
 		i.rotation += rotation_direction * Globals.ROTATION_SPEED * delta
 
 func change_selection():
 	var direction = Input.get_axis("select_prev", "select_next")
 	
-	if direction > 1:
+	if direction > 0:
 		selected_rotation_group += 1
 		if selected_rotation_group >= rotation_groups.size():
 			selected_rotation_group = 0
-	elif direction < 1:
+	elif direction < 0:
 		selected_rotation_group -= 1
 		if selected_rotation_group < 0:
 			selected_rotation_group = rotation_groups.size() -1
@@ -37,7 +36,4 @@ func _process(delta: float) -> void:
 		rotate_panels(delta);
 	
 	if Input.is_action_just_pressed("select_prev") or Input.is_action_just_pressed("select_next"):
-		print("switch detected!")
 		change_selection()
-		
-	pass
